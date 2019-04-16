0618392182079c5e220a2536bc3bfd99360ced1a
TeguhBudianto2 <48235251+TeguhBudianto2@users.noreply.github.com>
2019-04-17 05:27:11 +0700

    Update index.js

--- index.js
+++ index.js
@@ -4,23 +4,13 @@ const bot = new Discord.Client({disableEveryone: true});
 bot.on("ready", async () => {
   console.log(`${bot.user.username} is online!`);
     bot.user.setActivity("New! SERVER QQ BOT")
-  bot.user.setStatus('idle') // Can be 'available', 'idle', 'dnd', or 'invisible'
-  bot.user.setActivity("QQ HELP ME.",  {type: "LISTENING"}) ; // sets the activity of the bot. 
+  bot.user.setActivity("QQ HELP ME.",  {type: "LISTENING"}) ; 
+let activNum = 0; 
+set lnterval(function() {if {activNum === 0) {bot.user.setActivity{"with QQ Help server in"}; // changing it's on.
+activNum = 1;} else if (activNum === 1) {bot.user.setActivity("with online now.");
+activNum = 0;}
 
-// we need a variable to track on what activity it's on. 
-let activNum = 0; // zero is the first activity.
-
-// Tick event: Runs every so often the argument of the lnterval tells it to. 
-set lnterval(function() { if 
-{activNum === 0) { // checking on what activity it's on.
-bot.user.setActivity{"with QQ Help server in"}; // changing it's on.
-activNum = 1; // changing the tracking variable to change the activity later on.
-} else if (activNum === 1) { bot.user.setActivity("with online now."); // changing the activity again.
-activNum = 0; // changes back the tracking variable.
-}
-
-}, 3 * 1000); // Every 3 seconds. the '* 1000' is here so the  '3' is going to be converted from millisecords to seconds.
-});
+}, 3 * 1000);
   
 });
 
