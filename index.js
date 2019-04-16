f03b87f03daf2cee6de45aa6fb1ad2444f1a5d31
TeguhBudianto2 <48235251+TeguhBudianto2@users.noreply.github.com>
2019-04-17 05:36:12 +0700

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ bot.on("ready", async () => {
   bot.user.setActivity("QQ HELP ME.",  {type: "LISTENING"}); 
 setInterval(() => {
         const index = Math.floor(Math.random() * (activities_list.length - 1) + 1); // generates a random number between 1 and the length of the activities array list (in this case 5).
-        client.user.setActivity(activities_list[index]); // sets bot's activities to one of the phrases in the arraylist.
+        bot.user.setActivity(activities_list[index]); // sets bot's activities to one of the phrases in the arraylist.
     }, 10000); // Runs this every 10 seconds.
 });
 
