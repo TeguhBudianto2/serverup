d40c55e80c9a89aaebb9dfabcc1fc567c6e5d3ff
TeguhBudianto2 <48235251+TeguhBudianto2@users.noreply.github.com>
2019-04-17 05:32:23 +0700

    Update index.js

--- index.js
+++ index.js
@@ -4,14 +4,11 @@ const bot = new Discord.Client({disableEveryone: true});
 bot.on("ready", async () => {
   console.log(`${bot.user.username} is online!`);
     bot.user.setActivity("New! SERVER QQ BOT")
-  bot.user.setActivity("QQ HELP ME.",  {type: "LISTENING"}) ; 
-let activNum = 0; 
-set lnterval(function() {if {activNum === 0) {bot.user.setActivity{"with QQ Help server in"}; // changing it's on.
-activNum = 1;} else if (activNum === 1) {bot.user.setActivity("with online now.");
-activNum = 0;}
-
-}, 3 * 1000);
-  
+  bot.user.setActivity("QQ HELP ME.",  {type: "LISTENING"}); 
+setInterval(() => {
+        const index = Math.floor(Math.random() * (activities_list.length - 1) + 1); // generates a random number between 1 and the length of the activities array list (in this case 5).
+        client.user.setActivity(activities_list[index]); // sets bot's activities to one of the phrases in the arraylist.
+    }, 10000); // Runs this every 10 seconds.
 });
 
 bot.on("message", async message => {
