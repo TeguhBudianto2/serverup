1afde18a46849d011054d1063d9b68e7c4719939
TeguhBudianto2 <48235251+TeguhBudianto2@users.noreply.github.com>
2019-04-21 12:05:26 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 const Discord = require("discord.js");
 const bot = new Discord.Client({disableEveryone: true});
 
-let activityTypes = ['PLAYING','STREAMING','LISTENING','WATCHING']
+let activity = ["`${client.users.size} commands | ><help`, {type: "LISTENING"}", "`on ${client.guilds.size} servers | ><help`, {type: "PLAYING"}
+
 bot.on('ready', () => {
+
     setInterval(() => {
+
         const index = Math.floor(Math.random() * (activities_list.length - 1) + 1); // generates a random number between 1 and the length of the activities array list (in this case 5).
+
         bot.user.setActivity(activities_list[index]); // sets bot's activities to one of the phrases in the arraylist.
+
     }, 10000);
+
 });
 
 
