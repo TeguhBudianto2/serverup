17d241baefee7b41a360acf8717ff8d31f80771a
TeguhBudianto2 <48235251+TeguhBudianto2@users.noreply.github.com>
2019-04-21 10:41:01 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 const Discord = require("discord.js");
 const bot = new Discord.Client({disableEveryone: true});
 
-bot.on("ready", async () => {
-  console.log(`${bot.user.username} is online!`);
-    bot.user.setActivity("New! SERVER QQ BOT")
-  bot.user.setActivity("QQ HELP ME.",  {type: "WATCHING"});
+const activities_list = [
+    "with the &help command.", 
+    "with the developers console",
+    "with some code", 
+    "with JavaScript"
+    ]; // creates an arraylist containing phrases you want your bot to switch through.
+
+bot.on('ready', () => {
+    setInterval(() => {
+        const index = Math.floor(Math.random() * (activities_list.length - 1) + 1); // generates a random number between 1 and the length of the activities array list (in this case 5).
+        bot.user.setActivity(activities_list[index]); // sets bot's activities to one of the phrases in the arraylist.
+    }, 10000); // Runs this every 10 seconds.
 });
+
 
 bot.on("message", async message => {
 
