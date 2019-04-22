77029184b13c2b4560a1df7e5a180d6075c013b3
TeguhBudianto2 <48235251+TeguhBudianto2@users.noreply.github.com>
2019-04-23 05:36:45 +0700

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const bot = new Discord.Client({disableEveryone: true});
 bot.on("ready", async () => {
   console.log(`${bot.user.username} is online!`);
   bot.user.setActivity("YouTube: Teguh Gaming ID", {type: "STREAMING"})
-  bot.user.setActivity(`New update QQ!`);
 });
 
 
